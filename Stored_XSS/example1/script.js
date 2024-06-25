af4958c815ee53009920034fb157a17f0741e9b9
Raul <9953156+raul23@users.noreply.github.com>
2024-06-24 20:16:58 -0400

    Update script.js

--- Stored_XSS/example1/script.js
+++ Stored_XSS/example1/script.js
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>List Users</title>
+</head>
+<body>
+    <h1>List of Active Users</h1>
 
+    <div id="userlist">
+        Currently Active Users:
+        <?php
+        // Read user data from text file
+        $users = file('users.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        if ($users === false) {
+            exit('Error reading user data.');
+        }
+
+        // Print list of users to page
+        foreach ($users as $user) {
+            list($username, $password, $fullName) = explode(',', $user);
+            echo '<div class="userNames">' . $fullName . '</div>'; // No htmlspecialchars() used intentionally
+        }
+        ?>
+    </div>
+
+</body>
+</html>
